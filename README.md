9ee949845f5b4783b2f4e545b689095f2f55f7cf
gregorio <gfreidin@dc.uba.ar>
2020-10-04 19:55:08 -0300

    First commit, adding test folder and requirements

---
+++ README.md
@@ -0,0 +1,15 @@
+
+### Install music clasifier
+
+* Clone repo
+  `git clone git@github.com:GregorioF2/songs-clasifier.git`
+* Enter into folder
+  `cd songs-clasifier`  
+* Create python enviorment for Python 3.6
+  ```
+  virtualenv --python=/usr/bin/python3.6 venv
+  . venv/bin/activate
+  ```
+* Install requirements
+  ```
+  pip install -r requirements.txt
---
+++ test/test.py
@@ -0,0 +1,16 @@
+from pyAudioAnalysis import audioBasicIO
+from pyAudioAnalysis import ShortTermFeatures
+import matplotlib.pyplot as plt
+[Fs, x] = audioBasicIO.read_audio_file("./samples/track1_mono.wav")
+
+print('Fs: \n', Fs)
+print('\n\n\nx: ', len(x))
+print('\n\n\nx: ', x[4645760])
+
+F, f_names = ShortTermFeatures.feature_extraction(x, Fs, 0.050*Fs, 0.025*Fs)
+
+print('F: \n', F)
+print('\n\n\nf_names: ', f_names)
+
+plt.subplot(2,1,1); plt.plot(F[0,:]); plt.xlabel('Frame no'); plt.ylabel(f_names[0]) 
+plt.subplot(2,1,2); plt.plot(F[1,:]); plt.xlabel('Frame no'); plt.ylabel(f_names[1]); plt.show()

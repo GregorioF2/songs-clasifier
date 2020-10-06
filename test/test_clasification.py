40b0e517ac52982003d881966bbfc33fa4f99d93
gregorio <gfreidin@dc.uba.ar>
2020-10-06 17:50:22 -0300

    Add psy audio analysis venv and some samples

---
+++ test/test_clasification.py
@@ -0,0 +1,6 @@
+from pyAudioAnalysis import audioTrainTest as aT
+aT.extract_features_and_train(["./samples/like_themes/", "./samples/unlike_themes/"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "knn", "liked", True)
+
+print('\n\n Clasiffie archivo')
+
+aT.file_classification("./samples/test_themes/test_1.wav", "./liked", "knn")
